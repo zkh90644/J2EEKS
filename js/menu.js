f9f1a200fd4c21e05162523d4ba816e116904e04
Creator <Mikezhang1112@163.com>
2015-11-17 19:39:58 +0800

    test
    create

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+function ShowList () {
+	document.getElementById('z_div_menu_list').style.display="block";
+}
+
+function HideList () {
+	document.getElementById('z_div_menu_list').style.display="none";
+}
