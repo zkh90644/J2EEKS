01fdce04f2a55cfab573e9419f5054f746b7dd4d
Creator <Mikezhang1112@163.com>
2015-11-27 19:09:40 +0800

    重构代码并使用bootstrap进行布局
    因为bootstrap更加适用于响应式布局，减少需要考虑的问题。

--- js/menu.js
+++
@@ -1,7 +0,0 @@
-function ShowList () {
-	document.getElementById('z_div_menu_list').style.display="block";
-}
-
-function HideList () {
-	document.getElementById('z_div_menu_list').style.display="none";
-}
