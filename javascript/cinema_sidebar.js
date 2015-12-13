20ebb6379b4ac09ee2da373bd6acd4739d8b9da4
Creator <Mikezhang1112@163.com>
2015-12-13 19:31:07 +0800

    finish cinema_cinema_set.html

--- javascript/cinema_sidebar.js
+++ javascript/cinema_sidebar.js
@@ -24,18 +24,6 @@ function changeLeftOfContent(time) {
   }
 }
 
-// 滚动页面的时候更改界面
-$(document).ready(function () {
-  $(document).scroll(changeLeftOfContent);
-  $(window).resize(changeLeftOfContent);
-});
-
-// 页面加载完的时候第一次加载
-window.onload = function () {
-  changeLeftOfContent();
-}
-
-
 $(function () { $('#z_ul_cinema_set').collapse({toggle: false})});
 $(function () { $('#z_ul_film_set').collapse({toggle: false})});
 
